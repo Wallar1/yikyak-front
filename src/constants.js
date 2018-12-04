3528a4147772c5c80440946237407bbff5ae3552
Craig Wallace <Robertwallace.naples@gmail.com>
2018-12-03 22:07:19 -0800

    changing api route back to heroku

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-// export const API_ROOT = 'http://yikyakclonebackend.herokuapp.com';
-// export const API_WS_ROOT = 'wss://yikyakclonebackend.herokuapp.com/cable';
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'http://yikyakclonebackend.herokuapp.com';
+export const API_WS_ROOT = 'wss://yikyakclonebackend.herokuapp.com/cable';
+// export const API_ROOT = 'http://localhost:3000';
+// export const API_WS_ROOT = 'ws://localhost:3000/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
